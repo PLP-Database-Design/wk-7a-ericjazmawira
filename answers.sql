47af5096160aea51b57c90fc06c44e93e810ebb3
ericjazmawira <ericjazmawira@gmail.com>
2025-05-13 21:55:03 +0300

    WEEK 7 DB ASSIGNMENT

---
+++ answers.sql
@@ -0,0 +1,60 @@
+-- First we create a database to help work around the data
+CREATE DATABASE WEEK_SEVEN;
+
+USE week_seven;
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(255)
+);
+
+INSERT INTO ProductDetail (OrderID, CustomerName, Products) VALUES
+(101, 'John Doe', 'Laptop, Mouse'),
+(102, 'Jane Smith', 'Tablet, Keyboard, Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+SELECT * FROM ProductDetail;
+
+-- Question one
+SELECT 101 AS OrderID, 'John Doe' AS CustomerName, 'Laptop' AS Product
+UNION ALL
+SELECT 101, 'John Doe', 'Mouse'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Tablet'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Keyboard'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Mouse'
+UNION ALL
+SELECT 103, 'Emily Clark', 'Phone';
+
+-- Question two
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+INSERT INTO Orders (OrderID, CustomerName) 
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+CREATE TABLE OrderDetails (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+INSERT INTO OrderDetails (OrderID, Product, Quantity) 
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
+
+
